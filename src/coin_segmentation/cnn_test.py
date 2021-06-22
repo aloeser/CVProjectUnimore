face00b54cd24719014169bb3d9793dd8140bbab
Roman Staehle <roman.staehle@outlook.com>
2021-06-22 11:30:31 +0200

    adding function descriptions

--- src/coin_segmentation/cnn_test.py
+++ src/coin_segmentation/cnn_test.py
@@ -4,6 +4,11 @@ import torch
 from moresnet import MOREsNet
 
 def which_coin(pred_vector):
+    """
+    Gets predicted output vector and returns cent accordingly to highest probability.
+    :param pred_vector: CNN predicted vector
+    :return: cent value prediction of coin
+    """
     # {'1': 0, '10ct': 1, '1ct': 2, '2': 3, '20ct': 4, '2ct': 5, '50ct': 6, '5ct': 7}
     cents = [100, 10, 1, 200, 20, 2, 50, 5]
     pos = np.argmax(pred_vector)
@@ -12,6 +17,12 @@ def which_coin(pred_vector):
     return cent
 
 def run_model(model, inp_pic):
+    """
+    Takes a single 64x64x3 coin picture, runs the given model and predicts coin value.
+    :param model: model with pre-trained weights
+    :param inp_pic: input picture of single coin
+    :return: cent value prediction of coin
+    """
     model.eval()
     with torch.no_grad():
         pred_vector = np.array(model(inp_pic)[0])
@@ -20,12 +31,22 @@ def run_model(model, inp_pic):
     return cent
 
 def img_preprocessing(img):
+    """
+    Takes input image from coin segmentation output vector and preprocesses for CNN model.
+    :param img: single 64x64x3 coin image
+    :return: CNN ready input image
+    """
     img = img[..., ::-1] / 255
     input_img = torch.from_numpy(img.copy())
     input_img = torch.moveaxis(input_img, -1, 0).unsqueeze(0).float()
     return input_img
 
 def get_prediction(img):
+    """
+    Predicts the cent value for a single coin picture through running a given model including image preprocessing.
+    :param img: single 64x64x3 coin image
+    :return: cent value prediction of coin
+    """
     # load model and get predicted CNN output vector
     model = MOREsNet(in_channels=3, num_classes=8, load_pretrained='./moresnet.tar')
     input_pic = img_preprocessing(img)
