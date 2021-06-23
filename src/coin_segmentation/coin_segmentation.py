1a7a1aefb3930ac5ef724b90ebddfca93fa07e54
Alexander Löser <alexander.loeser@student.hpi.de>
2021-06-23 12:10:21 +0200

    failed experiments with hough ellipse

--- src/coin_segmentation/coin_segmentation.py
+++ src/coin_segmentation/coin_segmentation.py
@@ -3,6 +3,13 @@ import numpy as np
 import random
 import os
 
+import skimage
+from skimage import data, color, img_as_ubyte
+from skimage.feature import canny
+from skimage.transform import hough_ellipse
+from skimage.draw import ellipse_perimeter
+import matplotlib.pyplot as plt
+
 def random_retrieval_image(path='retrieval_dataset'):
     retrieval_files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
     retrieval_file = random.choice(retrieval_files)
@@ -96,13 +103,70 @@ def get_coins_from_retrieval_image(retrieval_img):
     coin_images = extract_coins(retrieval_img, circles)
     return coin_images
 
+def so(image):
+    # Load picture, convert to grayscale and detect edges
+    image_rgb = image#[:, :, ::-1] # convert image from RGB (skimage) to BGR (opencv)
+    image_gray_cv = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    image_gray = color.rgb2gray(image_rgb)
+    #cv.imshow("gray", image_gray)
+
+    t, _ = cv.threshold(image_gray_cv, 0, 255, cv.THRESH_OTSU)
+    std = image_gray_cv.std()
+    canny_max = min(255, t + 2 * std) / 255
+    canny_min = max(0, t - 2 * std) / 255
+    edges = canny(image_gray, sigma=.8, low_threshold=canny_min, high_threshold=canny_max)
+    print(edges.any())
+
+    edge_image = edges.astype(np.uint8) * 255
+    cv.imshow("canny", edge_image)
+
+    #return
+
+
+    # Perform a Hough Transform
+    # The accuracy corresponds to the bin size of a major axis.
+    # The value is chosen in order to get a single high accumulator.
+    # The threshold eliminates low accumulators
+    print("starting ellipse transform")
+    result = hough_ellipse(edges, min_size=10, max_size=15, accuracy=20, threshold=50)
+    print("done with ellipse transform")
+    print(result.shape)
+    result.sort(order='accumulator')
+
+    # Estimated parameters for the ellipse
+    best = list(result[-1])
+    yc, xc, a, b = [int(round(x)) for x in best[1:5]]
+    orientation = best[5]
+
+    # Draw the ellipse on the original image
+    cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
+    image_rgb[cy, cx] = (0, 0, 255)
+    # Draw the edge (white) and the resulting ellipse (red)
+    edges = color.gray2rgb(img_as_ubyte(edges))
+    edges[cy, cx] = (0, 0, 250)
+    cv.imshow('edges', img_as_ubyte(edges))
+    #return image_rgb
+    return img_as_ubyte(image_rgb)
+
 def main():
     retrieval_img = random_retrieval_image()
     #retrieval_img = cv.imread('/home/aloeser/Downloads/opencv2.png')
     cv.imshow("original", retrieval_img)
+
     coin_images = get_coins_from_retrieval_image(retrieval_img)
-    for index, image in enumerate(coin_images):
-        cv.imshow(f"Coin Image {index}", image)
+    #for index, image in enumerate(coin_images):
+    #    cv.imshow(f"Coin Image {index}", image)
+
+    # sift experiments
+    gray = cv.cvtColor(retrieval_img, cv.COLOR_BGR2GRAY)
+    sift = cv.SIFT_create()
+    kp = sift.detect(gray, None)
+    #img = cv.drawKeypoints(gray, kp, retrieval_img)
+    #cv.imshow("SIFT keypoints", img)
+
+    ellipses = so(retrieval_img)
+    cv.imshow("Hough Ellipse", ellipses)
+
     cv.waitKey()
     cv.destroyAllWindows()
 
